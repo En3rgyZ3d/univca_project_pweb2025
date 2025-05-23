750c5686560f2330b7e23311ce5da805b13a21ba
en3rgyz3d <jekivilmigliore@gmail.com>
2025-05-23 20:20:39 +0200

    feat(data): add data classes for database
    - add data classes for database, including "User" and "Event" classes

---
+++ app/models/user.py
@@ -0,0 +1,15 @@
+from sqlmodel import SQLModel, Field
+
+
+class UserBase(SQLModel): # Common attributes
+    name: str
+    email: str
+
+class User(UserBase,table = True): # Class used for ORM
+    username: str = Field(primary_key=True)
+
+class UserPublic(UserBase): # Class used to show User data
+    username: str
+
+class UserCreate(User): # Class used to create a user, imports everything from class User
+    pass
