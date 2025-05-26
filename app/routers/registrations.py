061e1deaa007451c149765046247bff58114aa2a
Dximon <139597017+Dximon@users.noreply.github.com>
2025-05-26 21:37:06 +0200

    feat(api): add registrations router - Add GET "/registrations" endpoint - Add GET "/registrations/{username}" endpoint - Add DELETE "/registrations/{username}/{event_id}" endpoint

---
+++ app/routers/registrations.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, Query, Path, HTTPException
+from sqlalchemy.orm import session
+
+from sqlmodel import select,delete
+from typing import Annotated
+
+from app.data.db import SessionDep
+from app.models.registration import Registration
+from app.models.event import Event, EventPublic
+from app.models.user import User, UserPublic
+
+router= APIRouter(prefix="/registrations")
+
+
+@router.get("/")
+def get_all_registrations(session: SessionDep) -> list[Registration]:
+    """Returns all registrations"""
+    statement = select(Registration)
+    registrations = session.exec(statement).all()
+    return registrations
+
+
+@router.get("/{username}")
+def get_user_registrations(session: SessionDep, username: Annotated[str, Path(description="The Username of the User to check")]) -> list[EventPublic]:
+    """Returns all registrations of a given user"""
+    statement = select(Event).join(Registration).where(Registration.username == username)
+    registrations = session.exec(statement).all()
+    return registrations
+
+
+@router.delete("/{username}/{event_id}")
+def delete_registration(session: SessionDep, username: Annotated[str, Path(description="The Username of the User to check")], event_id: Annotated[int, Path(description="The ID of the Event to check")]) -> str:
+    """Deletes a registration"""
+    statement = delete(Registration).where(Registration.username == username, Registration.event_id == event_id)
+    session.exec(statement)
+    session.commit()
+    return ("Registration deleted successfully!")
+
+
