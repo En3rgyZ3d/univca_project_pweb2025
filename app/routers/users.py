95b3611ae01902659e74403e8ac2108417e99de2
xxAllorixx <74657314+xxallorixx@users.noreply.github.com>
2025-05-27 11:04:17 +0200

    Merge pull request #4 from En3rgyZ3d/dev_db
    feat(db): added database init with fake data

--- app/routers/users.py
+++ app/routers/users.py
@@ -3,6 +3,7 @@ from sqlmodel import select,delete
 from typing import Annotated
 from app.data.db import SessionDep
 from app.models.user import UserPublic, User, UserCreate
+from app.models.registration import Registration
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -24,24 +25,33 @@ def delete_users(session: SessionDep):
     """Deletes all users from the list."""
     session.exec(delete(User))
     session.commit()
+
+    """Deletes all registrations from the list."""
+    session.exec(delete(Registration))
+    session.commit()
     return "Users successfully deleted."
 
 @router.get("/{username}", response_model=UserPublic)
 def get_user_by_username(username: Annotated[str, Path(description="Username of the user to search")], session: SessionDep):
     # Retrieve a user by username from the database.
-    user = session.exec(select(User).where(User.username == username)).first()
-    if not user:
-        raise HTTPException(
-            status_code= status.HTTP_404_NOT_FOUND,
-            detail="User not found"
-        )
-    return user
+    user = session.get(User, username) # User is the table, username is the PK.
+    if user: # If the user is found, we return it; otherwise we return error code 404.
+        return user
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
+
 
 @router.delete("/{username}")
 def delete_a_user(username: Annotated[str, Path(description="Username of the user to delete")], session: SessionDep):
     """Delete a user from the list."""
 
-    session.exec(delete(User).where(User.username == username))
+    user_to_delete = session.get(User, username)
+    session.delete(user_to_delete)
+    session.commit()
+
+    statement = delete(Registration).where(Registration.username == username)  # NOQA
+    # With NOQA, we are disabling warnings that are known bugs in type checks with SQLAlchemy (safe to ignore)
+    session.exec(statement)
     session.commit()
 
     return "User successfully deleted."
