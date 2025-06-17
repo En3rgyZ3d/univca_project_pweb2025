e5dbb5fe13e6ec0225c83c602be90d76700bfe6e
en3rgyz3d <jekivilmigliore@gmail.com>
2025-06-17 11:03:34 +0200

    fix(api): fix UserCreate validation bug

--- app/models/user.py
+++ app/models/user.py
@@ -3,12 +3,13 @@ from sqlmodel import SQLModel, Field
 class UserBase(SQLModel): # Common attributes
     name: str
     email: str
-
-class User(UserBase,table = True): # Class used for ORM
-    username: str = Field(primary_key=True)
-
-class UserPublic(UserBase): # Class used to show User data
     username: str
 
-class UserCreate(User): # Class used to create a user, imports everything from class User
+
+class User(UserBase,table = True): # Class used for ORM
+    username: str = Field(primary_key=True) # Overwrites the username field in UserBase
+
+class UserPublic(UserBase): # Class used to show User data
+    pass
+class UserCreate(UserBase): # Class used to create a user, imports everything from class User
     pass
